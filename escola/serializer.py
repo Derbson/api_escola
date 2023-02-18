7bb68fe10a4a8672774ce58b78a7b91249e1758f
Derbson <pedrin.olinda@gmail.com>
2023-02-17 21:11:54 -0300

    criando classes serializers

---
+++ escola/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Alunos, Curso
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Alunos
+        fields = ["id", "nome", "rg", "cpf", "data_nascimento"]
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        models = Curso
+        fields = "__all__"
+
+
